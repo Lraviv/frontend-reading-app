20bb34530b9f643a8ae4f29ea174bb02aa557964
LRaviv <59437803+Lraviv@users.noreply.github.com>
2025-03-07 01:55:57 +0200

    fix navbar

--- frontend-reading-app/src/components/Navbar.tsx
+++ frontend-reading-app/src/components/Navbar.tsx
@@ -4,16 +4,27 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <nav className="bg-blue-500 p-4">
-      <ul className="flex space-x-4 text-white">
-        <li>
-          <Link to="/" className="hover:text-gray-300">Home</Link>
+    <div >
+    <nav className=" p-4 ">
+      <ul className="flex border-b ">
+        <li className='-mb-px mr-1 '>
+          <Link to="/" className="bg-white inline-block py-2 px-4 text-blue-500 hover:text-blue-800 font-semibold mr-3">Home</Link>
         </li>
-        <li>
-          <Link to="/login" className="hover:text-gray-300">Login</Link>
+        <li className='mr-3'>
+          <Link to="/books" className="bg-white inline-block border-l border-t border-r rounded-t py-2 px-4 text-blue-700 font-semibold"> Books </Link>
+        </li>
+        <li className='mr-3'>
+          <Link to="/movies" className="bg-white inline-block py-2 px-4 text-blue-500 hover:text-blue-800 font-semibold"> Movies </Link>
+        </li>
+        <li className='mr-3'>
+          <Link to="/TV" className="bg-white inline-block py-2 px-4 text-blue-500 hover:text-blue-800 font-semibold"> TV Series </Link>
+        </li>
+        <li className='mr-3'>
+          <Link to="/Stats" className="bg-white inline-block py-2 px-4 text-blue-500 hover:text-blue-800 font-semibold"> Stats </Link>
         </li>
       </ul>
     </nav>
+    </div>
   )
 }
 
